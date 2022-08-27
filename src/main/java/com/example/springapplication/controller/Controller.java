00440d0c2822c7dff9362e29c9a128d1a42541f0
swatikanishu <nishuswatika@gmail.com>
2022-08-27 18:46:32 +0530

    Controller layer is added

--- src/main/java/com/example/springapplication/controller/Controller.java
+++ src/main/java/com/example/springapplication/controller/Controller.java
@@ -3,6 +3,7 @@ package com.example.springapplication.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
     @RestController
     public class Controller {
         @RequestMapping("/api")
