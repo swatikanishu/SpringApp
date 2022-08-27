3809df332d2f35ed5e9242f74c3f6a9dbf752ad9
swatikanishu <nishuswatika@gmail.com>
2022-08-27 19:05:31 +0530

    passed query parameter using get method

--- src/main/java/com/example/springapplication/controller/Controller.java
+++ src/main/java/com/example/springapplication/controller/Controller.java
@@ -1,14 +1,21 @@
 package com.example.springapplication.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
     @RestController
     public class Controller {
         @RequestMapping("/api")
-        public String Hello (){
+        public String Hello() {
             return "<h1>Hello From BridgeLabz!!</h1>";
+        }
+
+        @GetMapping("/query")
+        public String getUsers(@RequestParam(value = "name", defaultValue = "Swatika") String name) {
+            return "Hello! " + name + " From BridgeLabz!!";
         }
     }
 
